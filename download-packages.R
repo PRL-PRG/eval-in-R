e1dad326e839cfb3c2a41cfe41d2a68237432095
Pierre Donat-Bouillud <pierre.donat-bouillud@ircam.fr>
2020-06-04 11:55:20 +0200

    Better structure

--- download-packages.R
+++ download-packages.R
@@ -1,22 +1,47 @@
 # Download source code of most dowloaded R packages
-# (excluding tidyverse optionally)
+# or from a fst file
+# (excluding some packages optionally)
 
 library(tidyverse)
 library(cranlogs)
+library(fst)
 
 
-excluded_packages = c(tidyverse_packages(include_self = TRUE), "tidyselect")
-
-# Will change depending on the day...
-mostDownloadedPackages <- cran_top_downloads(count = 100) %>% 
-    filter(!package %in% excluded_packages ) 
-
-destdir = paste0("packages-", Sys.Date())
-dir.create(destdir)
-
-downloadedPackages <- download.packages(pkgs = pull(mostDownloadedPackages, package),  destdir = destdir, type = "source")
-
-for (package in downloadedPackages[,2])
+download_packages <- function(packages, excluded_packages=NULL) 
 {
-  untar(package, exdir=destdir)
+  destdir = paste0("packages-", Sys.Date())
+  dir.create(destdir)
+  
+  selectedPackages <- packages %>% filter(!package %in% excluded_packages)
+  
+  downloadedPackages <- download.packages(pkgs = pull(selectedPackages, package),  destdir = destdir, type = "source")
+  
+  for (package in downloadedPackages[,2])
+  {
+    untar(package, exdir=destdir)
+  }
 }
+
+full_tidyverse_packages <- function() 
+{
+  return(c(tidyverse_packages(include_self = TRUE), "tidyselect"))
+}
+
+# Without tidyverse packages
+crantidy_packages <- function()
+{
+  # Will change depending on the day...
+  mostDownloadedPackages <- cran_top_downloads(count = 100)
+  download_packages(mostDownloadedPackages, full_tidyverse_packages())
+}
+
+
+packages_from_fst <- function(fstfile, n = 100)
+{
+  packages <-   as_tibble(read.fst(fstfile)) %>% 
+    rename( package = `Package Name`) %>%
+    head(n)
+    
+  download_packages(packages)
+}
+
